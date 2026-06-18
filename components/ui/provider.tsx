cc1c90ff5b02f09755ccc5991961ed1b4afc17be
Miroslava Hreško <miroslava.hresko@gmail.com>
2026-06-18 19:49:41 +0200

    Solving the error by removing the color mode component entirely - the autogenerated color mode did not work and then the modified color mode component did not work, so I give up, because i do not need it for now anyway and once i will want it i will solve it later.
    Console Error
    
    Encountered a script tag while rendering React component. Scripts inside React components are never executed when rendering on the client.

--- components/ui/provider.tsx
+++ components/ui/provider.tsx
@@ -1,12 +1,11 @@
 'use client'
 
 import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
-import { ColorModeProvider, type ColorModeProviderProps } from './color-mode'
 
-export function Provider(props: ColorModeProviderProps) {
+export function Provider({ children }: { children: React.ReactNode }) {
   return (
     <ChakraProvider value={defaultSystem}>
-      <ColorModeProvider {...props} />
+      {children}
     </ChakraProvider>
   )
 }
